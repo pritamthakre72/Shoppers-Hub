33ead8b0a317f4af33034089c117bfe35500361f
Anurag <sharmaanurag2gmail.com>
2024-06-24 01:15:10 +0530

    fixed order status

--- Shoppers_Hub/app/models.py
+++ Shoppers_Hub/app/models.py
@@ -91,6 +91,7 @@ STATUS_CHOICES = (
   ('Delivered','Delivered'),
   ('Cancel','Cancel')
 )
+
 class OrderPlaced(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
